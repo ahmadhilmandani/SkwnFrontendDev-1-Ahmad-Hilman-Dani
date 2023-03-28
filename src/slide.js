5f659d83f718a5d2165679c025bf4f6894f42033
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-03-29 00:37:56 +0700

    UPDATE animation carosel at product

---
+++ src/slide.js
@@ -0,0 +1,9 @@
+// $('#carosoul').mousemove(function(e){
+//     // console.log('hai')
+
+//     let msg = "Mouse position: " + e.pageX + ", " + e.pageY;
+
+//     // $('#carosoul').scrollLeft = e.pageX
+
+//     console.log(msg);
+// });
